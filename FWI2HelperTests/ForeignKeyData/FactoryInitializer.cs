b34fb1b26a1d9122db08806757e3e1910370235d
Johannes Pinzer <pinzerj@bswiesaude.onmicrosoft.com>
2022-12-13 10:34:44 +0100

    Unit Tests angepasst

--- FWI2HelperTests/ForeignKeyData/FactoryInitializer.cs
+++ FWI2HelperTests/ForeignKeyData/FactoryInitializer.cs
@@ -6,21 +6,20 @@ public static class FactoryInitializer
 
     public static void InitializeFactories()
     {
-        if(_initialized) { return; }
+        if (_initialized) { return; }
 
         // Configure Factory
-        DBAccess<Artikel>.RegisterFactory((fact) => 
+        DBAccess<Artikel>.RegisterFactory((fact) =>
         {
             fact.CreateMapping("webshop_artikel")
                 .AddPrimaryKey(e => e.Id, "id", MySqlDbType.Int32)
                 .AddField(e => e.Bezeichnung, "bezeichnung", MySqlDbType.VarChar)
                 .AddField(e => e.Beschreibung, "beschreibung", MySqlDbType.Text)
                 .AddField(e => e.Kundenbewertung, "kundenbewertung", MySqlDbType.Int32)
-                .AddField(e => e.Preis, "preis", MySqlDbType.Decimal)
-                .AddField(e => e.Image, "image", MySqlDbType.Blob);
+                .AddField(e => e.Preis, "preis", MySqlDbType.Decimal);
         });
 
-        DBAccess<Kunde>.RegisterFactory((fact) => 
+        DBAccess<Kunde>.RegisterFactory((fact) =>
         {
             fact.CreateMapping("webshop_kunde")
                 .AddPrimaryKey(e => e.Id, "id", MySqlDbType.Int32)
@@ -30,7 +29,7 @@ public static class FactoryInitializer
                 .AddField(e => e.PasswortHash, "passwort", MySqlDbType.VarChar);
         });
 
-        DBAccess<Bestellung>.RegisterFactory((fact) => 
+        DBAccess<Bestellung>.RegisterFactory((fact) =>
         {
             // Configure Factory
             fact.CreateMapping("webshop_bestellung")
@@ -40,7 +39,7 @@ public static class FactoryInitializer
                 .AddForeignKey(e => (IEnumerable<BestellPos>)e.Positionen, "webshop_bestellpos", "idbestellung", MySqlDbType.Int32);
         });
 
-        DBAccess<BestellPos>.RegisterFactory((fact) => 
+        DBAccess<BestellPos>.RegisterFactory((fact) =>
         {
             fact.CreateMapping("webshop_bestellpos")
                 .AddPrimaryKey(e => e.Id, "id", MySqlDbType.Int32)
@@ -48,7 +47,7 @@ public static class FactoryInitializer
                 .AddForeignKeyImport<Bestellung>(e => e.Positionen, "webshop_bestellung", "idbestellung", MySqlDbType.Int32)
                 .AddField(e => e.Menge, "menge", MySqlDbType.Int32);
         });
-        
+
         _initialized = true;
-    }    
+    }
 }
